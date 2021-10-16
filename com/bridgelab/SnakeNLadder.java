84d1709c50e046b8030555e353ddc1202947a595
rahul yadav <ry219613@gmail.com>
2021-10-16 19:06:02 +0530

    calculate number in between 1 to 6

--- com/bridgelab/SnakeNLadder.java
+++ com/bridgelab/SnakeNLadder.java
@@ -2,11 +2,10 @@ package com.bridgelab;
 
 public class SnakeNLadder {
 	public static void main(String[] args) {
-				//variables
+		//variables
 				int positionPlayer1 = 0;
-			}
-	}
 
-	
-		
-	
+				// Random Dice value between 1-6
+				int dice_value = (int) Math.floor((Math.random()*6) + 1);
+			}
+		}
